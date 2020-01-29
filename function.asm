277e73828124b2b1e0690b9e60ecc51b7daad392
Bruno Bernardo <bruno@xhail.com>
2020-01-29 13:32:12 +0000

    fix function

--- function.asm
+++ function.asm
@@ -9,23 +9,28 @@ mov sp, bp
 
 push message
 call print_message
-jmp $
+
+jmp $                               ; return to itself eternally
 
 
 ;
 ; Functions
 ;
 print_message:
-    pop bx
-    pop si
-    lodsb
-    cmp al, 0
-    je exit_print_message    
-    mov ah, 0x0e
-    int 0x10
-    jmp print_message
+    pop bx                          ; pop return address to bx
+    pop si                          ; pop message address from stack
+
+    print_message_loop:
+        lodsb                       ; mov al, [si] ; inc si
+        cmp al, 0
+        je exit_print_message       ; if al is 0 then exit
+
+        mov ah, 0x0e                ; scrolling teletype interrupt
+        int 0x10
+        jmp print_message_loop
+
     exit_print_message:
-        push bx
+        push bx                     ; push return address to stack
         ret
 
 ;
