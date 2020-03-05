aa1f6eee3b02dd10ca536de14db79a3d84cffcdd
Bruno Bernardo <bigsby@live.com>
2020-03-05 00:08:43 +0000

    add comments

--- tests/nasm-intel-template/boot.asm
+++ tests/nasm-intel-template/boot.asm
@@ -1,8 +1,10 @@
-[org 0x7c00]
+[bits 16]                   ; set 16bits Real Mode
+[org 0x7c00]                ; set origin for memory offset where 
+                            ; the bootloader is loaded into memory
 
-%include "start.asm"
+%include "start.asm"        ; include custom code file
 
-jmp $
+jmp $                       ; loop "eternally"
 
-times 510-($-$$) db 0
-dw 0xaa55
+times 510-($-$$) db 0       ; fill up with zeros until 510
+dw 0xaa55                   ; bootloader magic number
--- tests/nasm-intel-template/start.asm
+++ tests/nasm-intel-template/start.asm
@@ -1,23 +1,23 @@
-mov ax, 0xb800
-mov ds, ax
-xor di, di
-mov si, MESSAGE
+mov ax, 0xb800                  ; load into ax to be able to load into ds
+mov ds, ax                      ; set ds (destination address segment) as the begining of video memory
+xor di, di                      ; zero destination index
+mov si, MESSAGE                 ; set source index to MESSAGE address
 
-move:
-    xor dx, dx
-    mov dl, [cs:si]
-    cmp dl, 0
+move:                           ; get next character
+    xor dx, dx                  ; zero dx
+    mov dl, [cs:si]             ; load value in current si into dl
+    cmp dl, 0                   ; check if it's 0 byte terminating string
 
 idle:
-    jz idle
+    jz idle                     ; if it's end of string loop "eternally"
 
-    mov [di], dl
-    inc di
-    mov [di], BYTE 0x1e         ; Blue background, Intensified Yellow foreground
-    inc di
-    inc si
-    jmp move
+    mov [di], dl                ; load value in dl (current character)
+    inc di                      ; increment destination address index
+    mov [di], BYTE 0x1e         ; load Blue background, Intensified Yellow foreground
+    inc di                      ; increment destination address index
+    inc si                      ; increment source address index
+    jmp move                    ; return to getting next character
 
 MESSAGE:
-    db 'Hello, Bigsby!', 0
+    db 'Hello, Bigsby!', 0      ; zero terminated string message
 
