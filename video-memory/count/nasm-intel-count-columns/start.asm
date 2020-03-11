7f622bd63d43abae3da6ee2595b238b089cfff1a
Bruno Bernardo <bigsby@live.com>
2020-03-11 21:24:35 +0000

    add comments

--- video-memory/count/nasm-intel-count-columns/start.asm
+++ video-memory/count/nasm-intel-count-columns/start.asm
@@ -1,36 +1,29 @@
-mov ax, 0xb800              ; start of video text memory
-mov ds, ax
+mov ax, 0xb800              ; start of video text memory at 0xb8000
+mov ds, ax                  ; and move it into ds (data destination segment)
 
-xor di, di
-mov si, MASK
-mov cx, 0x80
-
-next_character:
-    xor dx, dx
-    mov dl, [cs:si]
-    cmp dl, 0
-    jz reset_mask
-
-    mov [di], dl
-    inc di
-    mov [di], BYTE 0x1e
-    inc di            ; 80 columns
-    inc si
-    dec cx
-    jz end
-    jmp next_character
+xor di, di                  ; set di (destination index) to zero)
+mov cx, 0x80                ; set counter for how many charaters to write
 
 reset_mask:
-    mov si, MASK
-    jmp next_character
+    mov si, MASK            ; set si (source index) to where the text is
 
-end:
-    jmp $
+next_character:
+    xor dx, dx              ; zero dx (data register)
+    mov dl, [cs:si]         ; move the set the content of si to dx low byte 
+    cmp dl, 0               ; check if characher is 0 (end of string)
+    jz reset_mask           ; if so, go to returning to the begining of string
 
+    mov [di], dl            ; set the character in dl into destination location index
+    inc di                  ; move di up to next byte to set color and background
+    mov [di], BYTE 0x1e     ; set color and background (blue) and foreground (yellow)
+    inc di                  ; point to next memory location
+    inc si                  ; point to the next charater on the string
+    dec cx                  ; decrement the counter
+    jnz next_character      ; if counter is not zero go to process next character
 
-
+jmp $                       ; jump "eternally" to itself
 
 MASK:
-    db "1234567890", 0
+    db "1234567890", 0      ; the text to be displayed
 
 
